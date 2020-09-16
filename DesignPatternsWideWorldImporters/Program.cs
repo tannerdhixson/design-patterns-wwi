5a45c4d3d1adfe14da1278a0b7969bbfabcd6862
Tanner Hixson <tannerdhixson@gmail.com>
2020-09-16 17:35:38 -0500

    Created the solution

---
+++ DesignPatternsWideWorldImporters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatternsWideWorldImporters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
